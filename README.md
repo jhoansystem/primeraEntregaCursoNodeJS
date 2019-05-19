5e9da3697e6764a5b7a706b5642425bc830841ee
jhoansystem <jhoansystem@gmail.com>
2019-05-19 18:06:26 -0500

    Primera entrega

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 primeraEntregaCursoNodeJS
+
+
+*Instalar*
+
+npm install
+
+*Ejemplo de uso*
+
+nodemon principal
+nodemon principal inscribir --i=123456 --n=jhoan --c=1020407920
+
+
---
+++ principal.js
@@ -0,0 +1,117 @@
+const opciones={
+	id:{
+		demand:true,
+		alias:'i'
+	},
+	nombre:{
+		demand:true,
+		alias:'n'
+	},
+	cedula:{
+		demand:true,
+		alias:'c'
+	}
+
+}
+const argv = require('yargs')
+.command('inscribir','Inscribirme en el curso', opciones)
+.argv;
+
+const fs =require('fs');
+
+let cursos=[
+
+	{
+		id:'123456',
+		nombre:'NodeJS',
+		horas:32,
+		valor:0
+	},
+	{
+		id:'654321',
+		nombre:'Ingles',
+		horas:64,
+		valor:200000
+	},
+	{
+		id:'456123',
+		nombre:'Bolsa de valores',
+		horas:32,
+		valor:150000
+	}
+];
+
+
+let imprimirOferta=(cursos,posicion,retornar)=>
+{
+		let curso=cursos[posicion];
+		let Resultados='El curso se llama: '+curso.nombre+'\n'+
+						'Tiene una duración de : '+curso.horas+'\n'+
+						'Tiene un valor de : '+curso.valor+'\n'+
+						'Id de inscripción es: '+curso.id+'\n'+
+						'############################################'+'\n'+
+						'############################################'
+		if(retornar=='N')
+		{
+			setTimeout(function(){
+		 		console.log(Resultados);
+			},2000); 
+		}
+		else
+		{
+			return Resultados;
+		}
+
+
+}
+
+let imprimir=(cursos)=>
+{
+	console.log('Oferta d cursos');
+
+	imprimirOferta(cursos,0,'N');
+	setTimeout(function(){
+		imprimirOferta(cursos,1,'N');
+	},2000); 
+	setTimeout(function(){
+		imprimirOferta(cursos,2,'N');
+	},4000); 
+}
+
+
+
+
+if( Object.keys(argv._).length === 0)
+{
+	imprimir(cursos)
+}
+else
+{
+
+
+	let cursoInscribir = cursos.find( curso => curso.id == argv.id);
+	console.log(cursoInscribir);
+
+	let Resultados='El estudiante: '+argv.nombre+'\n'+
+						'con cedula: '+argv.cedula+'\n'+
+						'El curso se llama: '+cursoInscribir.nombre+'\n'+
+						'Tiene una duración de : '+cursoInscribir.horas+'\n'+
+						'Tiene un valor de : '+cursoInscribir.valor+'\n'+
+						'Id de inscripción es: '+cursoInscribir.id+'\n'+
+						'############################################'+'\n'+
+						'############################################'
+	fs.writeFile('matricula.txt',Resultados,(err)=>{
+		if(err) throw(err);
+
+		console.log('El archivo se ha creado exitosamente "matricula.txt"');
+		console.log('Con el siguiente texto');
+		console.log('############################################'+'\n'+
+						'############################################')
+		console.log(Resultados);
+	})
+}
+
+
+
+
+
